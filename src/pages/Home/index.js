c78ccd7f975df6c40b459c996f5f371a5085569d
luizpaulogroup <bylpgaming@gmail.com>
2020-04-16 21:12:48 -0300

    Add styles row movie

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, FlatList, View, Text } from 'react-native';
+
+import RowMoview from './styles';
+
+import api from '../../services/api';
 
 export default function Home() {
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        get();
+    });
+
+    const get = async () => {
+
+        try {
+
+            const response = await api.get();
+            
+            setMovies(response.data.data.movies);
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
-        <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <View>
-                <Text>Home</Text>
-            </View>
+        <SafeAreaView style={{ flex: 1 }}>
+            <FlatList
+                data={movies}
+                keyExtractor={item => String(item.id)}
+                renderItem={({ item }) => <RowMoview item={item} />} />
         </SafeAreaView>
     );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+export default function RowMoview({ item }) {
+
+    const start = () => {
+        var stars = "";
+        return stars.padStart(item.rating, "★");
+    }
+
+    return (
+        <View style={styles.content}>
+            <Image
+                style={styles.image}
+                source={{ uri: item.medium_cover_image }}
+                resizeMode="cover" />
+            <View style={styles.info}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.year}>{`Ano: ${item.year}`}</Text>
+                <Text style={{ ...styles.rating, color: item.rating > 6 ? 'white' : 'red' }}>{`Nota: ${item.rating}`}</Text>
+                <Text style={styles.stars}>{start()}</Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    content: {
+        height: 120,
+        alignSelf: 'stretch',
+        marginHorizontal: 5,
+        marginVertical: 5,
+        flexDirection: 'row',
+    },
+    image: {
+        width: 100,
+        height: '100%'
+    },
+    info: {
+        paddingLeft: 5,
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    year: {
+        color: '#999',
+        fontSize: 14,
+        marginVertical: 2,
+    },
+    rating: {
+        marginVertical: 2,
+    },
+    stars: {
+        color: 'yellow',
+        marginVertical: 2,
+    }
+});
